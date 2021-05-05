515240b870d6687958eb99a702c8a18538d34158
KhairShkokany <>
2021-05-05 15:14:08 +0000

    add jv

---
+++ app.js
@@ -0,0 +1,43 @@
+var name ="My Anime List of Khair Shkokany";
+var type ="Welcome to this page "
+
+alert(type + "" + name);
+
+
+
+var qus ="the best anime is Attack on titan";
+var no = "say cancel or i will kill you";
+confirm(  qus + no  );
+
+ var anime=prompt('attack on titan or tokyo ghoul..!');
+
+// there is a way to change to lower case.
+
+
+if(anime=='titan'){
+  document.write('<img src="https://static2.cbrimages.com/wordpress/wp-content/uploads/2021/01/Attack-on-Titan-Season-4-poster-Eren-header.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5" width="250px">');
+}else if(anime === 'tokyo ghoul'){
+  document.write('<img src="https://ae01.alicdn.com/kf/HTB15dwhNVXXXXbIXXXXq6xXFXXXz/Tokyo-Ghoul-Anime-Kaneki-Ken-120-200CM-Single-layer-Blanket-37481.jpg_Q90.jpg_.webp" width="250px">');
+}else{
+  document.write('<h4> please  write attack or tokyo ghoul</h3>');
+}
+
+
+var type=prompt('whats your type of anime ?');
+
+if    ( type=='drama'){
+  console.log('do not lie on april')
+}else if  (type=='action'){
+  console.log('attack on titan')
+}else if  (type=='mix'){
+
+  console.log ('jujutsu no kaysi')
+}else{
+  document.write('<h3>can you plesae do not watch anime and go to turkey series and thank you</h3>')
+}
+
+
+
+
+
+
